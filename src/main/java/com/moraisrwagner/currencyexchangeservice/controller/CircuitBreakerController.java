d01491162697131a5283d12aa03561de8efc4549
wagner rodakevicz de morais <wagnerrdemorais@gmail.com>
2023-02-28 11:36:36 -0300

    Implementing Rate Limiter and Bulkhead example

--- src/main/java/com/moraisrwagner/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/moraisrwagner/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.moraisrwagner.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +22,9 @@ public class CircuitBreakerController {
     }
 
     @GetMapping("/sample-api-circuit-breaker")
-    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name = "default", fallbackMethod = "hardcodedResponse")
+//    @Bulkhead(name= "default")
     public String sampleApiCircuitBreaker() {
         log.info("Sample Api Circuit Breaker call");
         var response = new RestTemplate().getForEntity("http://localhost:8080/nothing", String.class);
