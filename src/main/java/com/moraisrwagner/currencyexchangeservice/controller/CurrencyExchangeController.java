90253f3c8213797bcc3d22cf4aa029a5f9ea86f0
wagner rodakevicz de morais <wagnerrdemorais@gmail.com>
2023-04-25 19:02:12 -0300

    Enable tracing with micrometer and zipkin

--- src/main/java/com/moraisrwagner/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/moraisrwagner/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -3,12 +3,12 @@ package com.moraisrwagner.currencyexchangeservice.controller;
 import com.moraisrwagner.currencyexchangeservice.bean.CurrencyExchange;
 import com.moraisrwagner.currencyexchangeservice.service.CurrencyExchangeService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 @RestController
 @AllArgsConstructor
@@ -21,8 +21,11 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
-
         var port = environment.getProperty("local.server.port");
+
+        Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+        logger.info("request with info from: {}, to {}", from, to);
+
         CurrencyExchange byFromAndTo = currencyExchangeService.findByFromAndTo(from, to);
         byFromAndTo.setEnvironment(port);
         return byFromAndTo;
