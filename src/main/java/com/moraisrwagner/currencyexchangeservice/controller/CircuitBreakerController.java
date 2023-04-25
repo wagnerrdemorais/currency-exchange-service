1c0b2a259545f1703ea6950728351694be7232a5
wagner rodakevicz de morais <wagnerrdemorais@gmail.com>
2023-04-25 19:35:45 -0300

    Enable tracing with micrometer and zipkin, configuring log pattern level, configuring restTemplate to work with micrometer

--- src/main/java/com/moraisrwagner/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/moraisrwagner/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,7 +1,5 @@
 package com.moraisrwagner.currencyexchangeservice.controller;
 
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
--- src/main/java/com/moraisrwagner/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/moraisrwagner/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -9,6 +9,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @AllArgsConstructor
@@ -16,6 +17,9 @@ public class CurrencyExchangeController {
 
     private final Environment environment;
     private final CurrencyExchangeService currencyExchangeService;
+
+    private final RestTemplate restTemplate;
+
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
